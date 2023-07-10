b004ae4eec32047c72ce4dd456ee11162640a831
pennyboys <keitopology0107@gmail.com>
2023-07-10 23:34:31 +0900

    secound commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+from PIL import Image
 
 st.title('Streamlit 超入門')
 st.write('プログレスバーの表示')
@@ -30,9 +31,9 @@ condition = st.slider('あなたの今の調子は？', 0, 100, 50)
 'あなたの趣味：', text
 'コンディション：', condition
 
-# if st.checkbox('Show Image'):
-#     img = Image.open('sample.JPG')
-#     st.image(img, caption=' Sarina Ushio', use_column_width=True)
+if st.checkbox('Show Image'):
+    img = Image.open('sample.JPG')
+    st.image(img, caption=' Sarina Ushio', use_column_width=True)
 
 # df = pd.DataFrame(
 #     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
